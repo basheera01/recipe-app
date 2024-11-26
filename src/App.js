5145c520a15dfe154d2ad81b68f5c4a6053709de
basheera01 <shaikbasheeraa@gmail.com>
2024-11-26 22:34:32 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import RecipeList from './components/ReceipeList';
 import RecipeDetails from './components/ReceipeDetails';
 import './App.css';
@@ -9,7 +9,7 @@ const App = () => {
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/recipes" element={<RecipeList/>}/>
+          <Route path="/recipes" element={<RecipeList />} />
           <Route path="/details/:id" element={<RecipeDetails />} />
         </Routes>
       </div>
